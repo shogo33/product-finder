3fbc98b9f11ad35c04bb884721e7d6e3aca0edcc
shogo33 <58657496+shogo33@users.noreply.github.com>
2026-05-17 20:50:18 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // 👈 Vercelでのファイルの読み込み迷子を防ぐ魔法の1行
   resolve: {
     dedupe: ['react', 'react-dom']
   }
