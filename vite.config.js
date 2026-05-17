a52ca23a7b35a7f99377b59dafffd6a8894f0b82
shogo33 <58657496+shogo33@users.noreply.github.com>
2026-05-17 22:03:23 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path' // 👈 追加
 
-// https://vite.dev
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  root: path.resolve(__dirname, './'), // 👈 Vercelにコードの場所を教える魔法の1行
 })
